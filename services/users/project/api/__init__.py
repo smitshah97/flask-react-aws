6abb6fddad273da0396202749aea54c70fb80cc9
smitshah97 <smitshah397@gmail.com>
2020-04-20 20:49:26 +0530

    Requirements file changed

--- services/users/project/api/__init__.py
+++ services/users/project/api/__init__.py
@@ -8,7 +8,7 @@ from project.api.users.views import users_namespace
 from project.api.sample import sample_namespace
 api = Api(version="1.0", title="Users API", doc="/doc/")
 
-api.add_namespace(sample_namespace,path="/sample")
+api.add_namespace(sample_namespace, path="/sample")
 api.add_namespace(ping_namespace, path="/ping")
 api.add_namespace(users_namespace, path="/users")
 api.add_namespace(auth_namespace, path="/auth")
